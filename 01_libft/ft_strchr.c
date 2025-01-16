aba7bee22ff7b1111654b6c1aa2775553e1f750f
J4P032 <J4p032@gmail.com>
2025-01-16 16:25:40 +0100

    strchr corregido overflow char

--- 01_libft/ft_strchr.c
+++ 01_libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 10:17:39 by jrollon-          #+#    #+#             */
-/*   Updated: 2025/01/16 12:47:09 by jrollon-         ###   ########.fr       */
+/*   Updated: 2025/01/16 16:24:37 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,22 @@
 /*if found -> returns pointer to first found in s*/
 /*not found -> NULL*/
 /*c = '\0' returns the first terminator*/
+/*needs to cast (char) because overflow conversion c = 1024 = \0
+	c = 'e'+256 = 'e'*/
+/*(char *) because it is a non const return from const char* */
 char	*ft_strchr(const char *s, int c)
 {
-	size_t 	i;
-	
+	size_t	i;
+
 	i = 0;
 	while (s[i])
 	{
-		if (s[i] == c)
+		if (s[i] == (char)c)
 			return ((char *)&s[i]);
 		i++;
 	}
-	if (c == '\0')
+	if ((char)c == '\0')
 		return ((char *)&s[i]);
 	else
-		return NULL;
+		return (NULL);
 }
