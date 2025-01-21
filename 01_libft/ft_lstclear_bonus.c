c82e645ba3e053c82ae9db867b386d77d5df93a0
J4P032 <J4p032@gmail.com>
2025-01-21 21:22:24 +0100

    Add files via upload

--- 01_libft/ft_lstclear_bonus.c
+++ 01_libft/ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 19:44:11 by jrollon-          #+#    #+#             */
-/*   Updated: 2025/01/21 20:49:40 by jrollon-         ###   ########.fr       */
+/*   Updated: 2025/01/21 21:17:50 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,22 +28,3 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 	}
 	*lst = NULL;
 }
-
-/*
-   t_list *current;
-    t_list *next;
-
-    if (!lst || !*lst)
-        return;
-
-    current = *lst;
-    while (current)
-    {
-        next = current->next;    // Guarda el siguiente antes de borrar
-        if (del)
-            del(current->content);  // Borra el contenido
-        free(current);           // Borra el nodo actual
-        current = next;          // Avanza al siguiente
-    }
-    *lst = NULL;                // Al final, pone el puntero a NULL
- */
