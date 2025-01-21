9e01d7e0775b7839dc82cfa4c589e6ebeaf1e7d9
J4P032 <J4p032@gmail.com>
2025-01-21 21:10:41 +0100

    Add files via upload

--- 01_libft/ft_bzero.c
+++ 01_libft/ft_bzero.c
@@ -6,11 +6,11 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 10:47:13 by jrollon-          #+#    #+#             */
-/*   Updated: 2025/01/15 11:18:11 by jrollon-         ###   ########.fr       */
+/*   Updated: 2025/01/20 08:26:13 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <stdlib.h>
 
 /*set to '\0' all n memory places starting by address pointed by s*/
 void	ft_bzero(void *s, size_t n)
