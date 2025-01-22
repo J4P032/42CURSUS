7e5acf8a1386de1e6ad50cf5473beeedb73832e5
J4P032 <J4p032@gmail.com>
2025-01-22 21:08:45 +0100

    Add files via upload

--- 01_libft/ft_memmove.c
+++ 01_libft/ft_memmove.c
@@ -6,21 +6,22 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 15:48:15 by jrollon-          #+#    #+#             */
-/*   Updated: 2025/01/20 16:42:49 by jrollon-         ###   ########.fr       */
+/*   Updated: 2025/01/22 20:42:53 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
 /*copy data from src to dest through n bytes but garanties good copy of all*/
+/*if 'dest' is before in memory than 'src' goes from last to first up to 'n'*/
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	size_t			i;
 	unsigned char	*aux_dest;
 	unsigned char	*aux_src;
 
-	if (!dest && !src)
-		return (NULL);
+	//if (!dest && !src)
+	//	return (NULL);
 	aux_dest = (unsigned char *)dest;
 	aux_src = (unsigned char *)src;
 	i = 0;
