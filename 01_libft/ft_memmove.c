9b16021f5264e68be049dbbbdc21321932e00181
J4P032 <J4p032@gmail.com>
2025-01-26 18:04:37 +0100

    ft_memmove fix cuando son iguales (devuelve destino)

--- 01_libft/ft_memmove.c
+++ 01_libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 15:48:15 by jrollon-          #+#    #+#             */
-/*   Updated: 2025/01/22 21:09:42 by jrollon-         ###   ########.fr       */
+/*   Updated: 2025/01/26 18:02:07 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	aux_dest = (unsigned char *)dest;
 	aux_src = (unsigned char *)src;
 	i = 0;
+	if (dest == src)
+		return (dest);
 	if (dest > src)
 	{
 		while (n--)
