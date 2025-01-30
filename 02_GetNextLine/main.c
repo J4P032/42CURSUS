8a547bba9cfc938f3e42f566ba26a2ea36eb650e
J4P032 <J4p032@gmail.com>
2025-01-30 23:43:08 +0100

    nuevo token github

--- 02_GetNextLine/main.c
+++ 02_GetNextLine/main.c
@@ -6,7 +6,7 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 18:18:57 by jrollon-          #+#    #+#             */
-/*   Updated: 2025/01/30 00:37:29 by jrollon-         ###   ########.fr       */
+/*   Updated: 2025/01/30 23:29:08 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,18 @@ int	main(int argc, char **argv)
 		return (1); */
 	(void)argc;
 	(void)argv;
-	fd = open("TEST/kk.txt", O_RDONLY);
+	fd = open("TEST/dib4.txt", O_RDONLY);
 	if (fd == -1)
 		write(2, "Error reading\n", 14);
-	linea = get_next_line(fd);
+	
+	for (size_t i = 0; i < 20; ++i)
+	{
+		linea = get_next_line(fd);
+		printf("%s", linea);
+	}
+	
+	/* linea = get_next_line(fd);
 	printf("\nLinea: %s", linea);
-	linea = get_next_line(fd);
-	printf("\nLinea: %s", linea);
-	close(fd);
+	 */close(fd);
 	return (0);
 }
