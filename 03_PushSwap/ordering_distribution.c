8750d12d8cd76ef70f5278254fe5cc3290781a39
J4P032 <J4p032@gmail.com>
2025-02-28 14:08:35 +0100

    add copia de stackA fuerza bruta

--- 03_PushSwap/ordering_distribution.c
+++ 03_PushSwap/ordering_distribution.c
@@ -6,11 +6,33 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 12:07:23 by jrollon-          #+#    #+#             */
-/*   Updated: 2025/02/28 12:43:52 by jrollon-         ###   ########.fr       */
+/*   Updated: 2025/02/28 14:07:28 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	copy_node(t_stack *a, t_stack **a_copy, t_stack *node)
+{
+	t_stack	*aux;
+
+	aux = NULL;
+	node->value = a->value;
+	node->index = a->index;
+	if ((!*a_copy))
+	{
+		node->next = NULL;
+		node->prev = NULL;
+		*a_copy = node;
+		return ;
+	}
+	aux = *a_copy;
+	while (aux->next)
+		aux = aux->next;
+	aux->next = node;
+	node->next = NULL;
+	node->prev = aux;
+}
 
 int	check_already_ordered(t_stack *a, size_t nodes)
 {
@@ -27,15 +49,49 @@ int	check_already_ordered(t_stack *a, size_t nodes)
 	return (1);
 }
 
+t_stack	*copy_stack(t_stack *a, size_t num_nodes)
+{
+	t_stack	*a_copy;
+	t_stack *node;
+	size_t	i;
 
+	i = 0;
+	a_copy = NULL;
+	while (i < num_nodes)
+	{
+		node = ft_calloc(1, sizeof(t_stack));
+		if (!node)
+		{
+			ft_free((void **)&a_copy, 1); //mirar si lo hace bien en caso de fallo de memoria
+			return (NULL);
+		}
+		copy_node (a, &a_copy, node);
+		i++;
+		a = a->next;
+	}
+	return (a_copy);
+}
+
+
+#include <stdio.h> ///
 void	ordering(t_stack **a, t_stack **b)
 {
+	t_stack	*a_copy;
 	size_t	chunck;
 	size_t	nodes;
 
 	(void)chunck;
+	(void)*b;
 	nodes = stacksize(*a);
 	if(check_already_ordered(*a, nodes))
 		return ;
+	a_copy = copy_stack(*a, nodes);
+	if (!a_copy)
+		return ;
+	if (nodes < 20)
+		return ; ///
+	
+	ft_free((void **)&a_copy, 1);
+	
 	ksort(a, b);
 }
