b9a520246273ac201b6c977ffed00c0597d388b3
J4P032 <J4p032@gmail.com>
2025-03-05 13:05:20 +0100

    Add files via upload

--- 03_SoLong/so_long.c
+++ 03_SoLong/so_long.c
@@ -6,24 +6,11 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 11:09:02 by jrollon-          #+#    #+#             */
-/*   Updated: 2025/03/04 20:13:43 by jrollon-         ###   ########.fr       */
+/*   Updated: 2025/03/05 13:02:32 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./minilibx-linux/mlx.h"
-#include <math.h>
-#include <stdlib.h>
-#define WIDTH 1024
-#define HEIGHT 1024
-
-typedef struct s_data
-{
-    void	*img;
-    char	*addr;
-    int		bits_per_pixel;
-    int		line_length;
-	int		endian;
-}           t_data;
+#include "so_long.h"
 
 int	close_win(void *param)
 {
@@ -32,47 +19,44 @@ int	close_win(void *param)
 	return (0);
 }
 
+/*function that "push" pixels into the screen*/
+/*we receive the data struct of the image created (a canvas)...*/
+/*...and obtain the memory of start of it with img->addr that comes from...*/
+/*...mlx_get_data_addr. BUT instead of address of x,y pixel = y * 1920 + x...*/
+/*...ex: &(2,1)pixel =  1 * 1920 + 2 = 1922 "index" = 1922 x 4bytes/pixel... */
+/*...each line of the 1080 lines it is not 1920 x 4 = 7680 bytes but more...*/
+/*...because minilibx way of doing. Real number is img.line_length sooooo ...*/
+/*...address(char to add 1byte) is &image + y * line_length + x * (...)'cause*/
+/*...RGB are 3 bytes = 24bites, ARGB = 4 bytes = 32 bites so as each pixel...*/
+/*...is 4bytes so 8 * 4 = 32bites bits_x_pixel = 32 or 24 / 8 gives bytes.*/
+/**(unsigned int*)pxl_adr = color once we have the &mem, we assign value of..*/
+/*...it with full red 0xff000000(RGBA). Minilibx don't allow alpha BUT...*/
+/*...we can ignore it. We could use 24bits(RGB)but should assign each pixel..*/
+/*...as: pxl_adr[0] = color & 0xFF //lowbyte blue*/
+/*...pxl_adr[1] = (color >> 8) & 0xFF; //middle byte green */
+/*...pxl_adr[2] = (color >> 16) & 0xFF; //higher byte red */
+/*...we move >> the bytes to the lower value to modify them*/
+void	put_pixel(t_data *img, int x, int y, int color)
+{
+	char	*pxl_adr;
+
+	pxl_adr = img->addr + (y * img->line_length + x * (img->bits_x_pixel / 8));
+	*(unsigned int*)pxl_adr = color;
+}
 
 int main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
-	int		x;
-	int		y;
-	int		color;
+	t_data	img;
 
-	//t_data	img;
-	x = 50;
-	y = 50;
-	color = 0xff0000;
 	mlx = mlx_init();
-	if (!mlx)
-		return (1);
-	/* img.img = mlx_new_image(mlx, 1929, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-					&img.endian); */
-
-	mlx_win = mlx_new_window(mlx, WIDTH, HEIGHT, "titulitis");
-	if(!mlx_win)
-		return (1);
-
-	double k = 0;
-	while (y < HEIGHT / 2)
-	{
-		x = 50;
-		k = 0;
-		color = 0xf00000;
-		while (x < WIDTH)
-		{
-			mlx_pixel_put(mlx, mlx_win, x,  sin(k) + tan(k) * y, color);
-			color += 0.0001 * 0x010000;
-			x++;
-			k = k + 0.001;
-		}
-		y++;
-	}
-
-
+	mlx_win = mlx_new_window(mlx, WIDTH, HEIGHT, "So Long");
+	img.img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_x_pixel, &img.line_length,
+					&img.endian);
+	put_pixel(&img, WIDTH / 2, HEIGHT / 2, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_hook(mlx_win, 17, 0, close_win, mlx_win);
 	mlx_loop(mlx);
 }
---
+++ 03_SoLong/so_long.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/05 12:55:02 by jrollon-          #+#    #+#             */
+/*   Updated: 2025/03/05 13:01:56 by jrollon-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+# include "./minilibx-linux/mlx.h"
+# include <math.h>
+# include <stdlib.h>
+
+# define WIDTH 1920
+# define HEIGHT 1080
+
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_x_pixel;
+	int		line_length;
+	int		endian;
+}			t_data;
+
+#endif
