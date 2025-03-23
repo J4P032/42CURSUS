54b91477ddcd30f4c40e418d386cd1c57ebb6a24
J4P032 <J4p032@gmail.com>
2025-03-23 01:08:14 +0100

    mejor ghost paredes

--- 03_SoLong/frame_phantom_bonus.c
+++ 03_SoLong/frame_phantom_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/22 14:49:08 by jrollon-          #+#    #+#             */
-/*   Updated: 2025/03/22 22:12:30 by jrollon-         ###   ########.fr       */
+/*   Updated: 2025/03/23 01:07:48 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,13 @@ void	phantom_path_finding(t_game *game)
 {
 	int	i;
 	int	j;
-
 	
 	i = game->win->sprite[14].y / SPRITE_HEIGHT;
 	j = game->win->sprite[14].x / SPRITE_WIDTH;
-
-
+	if (game->win->sprite[14].y % SPRITE_HEIGHT != 0)
+		i = i + 1;
+	if (game->win->sprite[14].x % SPRITE_WIDTH != 0)
+		j = j + 1;
 	if (game->map_cpy[i][j + 1] == '0')
 		game->map_cpy[i][j + 1] = 'g';
 	else if (game->map_cpy[i][j - 1] == '0')
@@ -77,14 +78,14 @@ void	phantom_anim(t_game *game)
 	phantom_go(game);
 	x = game->win->sprite[14].j * SPRITE_WIDTH;
 	y = game->win->sprite[14].i * SPRITE_HEIGHT;
-	if (game->win->sprite[14].x != x && i % 3 == 0)
+	if (game->win->sprite[14].x != x && i % 10 == 0)
 	{
 		if (game->win->sprite[14].x < x)
 			game->win->sprite[14].x++;
 		else if (game->win->sprite[14].x > x)
 			game->win->sprite[14].x--;
 	}
-	else if (game->win->sprite[14].y != y && i % 3 == 0)
+	else if (game->win->sprite[14].y != y && i % 10 == 0)
 	{
 		if (game->win->sprite[14].y < y)
 			game->win->sprite[14].y++;
@@ -93,11 +94,14 @@ void	phantom_anim(t_game *game)
 	}
 	phantom_sprite_anim(game, game->win->sprite[14].x, game->win->sprite[14].y);
 
-	for (size_t i = 0; i < game->map->lines; i++) //
+	printf("xtarget: %d\n", x);//
+	printf("x: %d\n", game->win->sprite[14].x);//
+
+/* 	for (size_t i = 0; i < game->map->lines; i++) //
 	{
 		printf("%s", game->map_cpy[i]);//
 	}
-	printf("\n");//
+	printf("\n");// */
 
 	///VA POR PAREDES. EL PROBLEMA ES QUE EN CUANTO PASA EL ORIGEN 0,0 DEL FANTASMA A LA OTRA CASILLA, YA CONSIDERA QUE ESTA EN ZONA
 	//SIN PAREDES, CUANDO TODAVIA ESTA. HAY QUE IMPLEMENTAR LO MISMO QUE EL PACMAN. EL ORIGINEN +64 EN Y y origen + 64  en X y que sea
