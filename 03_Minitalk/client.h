41663443abdd347a4f523f49d9da0b1b1ec3ee59
J4P032 <J4p032@gmail.com>
2025-03-28 11:39:06 +0100

    definicion h cliente

--- 03_Minitalk/client.h
+++ 03_Minitalk/client.h
@@ -1,22 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
+/*   client.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/28 10:54:07 by jrollon-          #+#    #+#             */
-/*   Updated: 2025/03/28 11:31:47 by jrollon-         ###   ########.fr       */
+/*   Created: 2025/03/28 11:32:04 by jrollon-          #+#    #+#             */
+/*   Updated: 2025/03/28 11:37:32 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "client.h"
+#ifndef	CLIENT_H
+# define CLIENT_H
+# include <signal.h>
+# include "./ft_printf/ft_printf.h"
 
-int	main(int ac, char **av)
-{
-	if (ac != 3)
-		return (ft_printf("Error:\nNeed: './client PID \"string\"'\n"), 1);
-	(void)av;
-	return (0);
-	
-}
+# define B_0 SIGUSR1
+# define B_1 SIGUSR2
+
+#endif
