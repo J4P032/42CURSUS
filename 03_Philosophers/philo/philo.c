69f38c51544e8d5c647e3e8b5f06afada12e9045
Javier Rollon <J4p032@gmail.com>
2025-04-09 18:03:42 +0200

    Free perfecto incluso cuando falla malloc

--- 03_Philosophers/philo/philo.c
+++ 03_Philosophers/philo/philo.c
@@ -6,23 +6,117 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 13:38:23 by jrollon-          #+#    #+#             */
-/*   Updated: 2025/04/08 14:20:35 by jrollon-         ###   ########.fr       */
+/*   Updated: 2025/04/09 18:01:46 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+t_philo	*give_me_last(t_game *game)
+{
+	t_philo	*aux;
+	
+	aux = game->philo;
+	while (aux->next)
+		aux = aux->next;	
+	return (aux);
+}
+
+/*gettimeofday saves time now in sec and microsec in struct start_game_time...*/
+/*...timeval struct is .tv_sec (*1000 to milisecs) and .tv_usec...*/
+/*...(/1000 to milisecs)*/
+/*I create a circular list*/
+int	init_philo(t_game *game)
+{
+	size_t	i;
+	t_philo	*aux;
+	t_philo *last;
+
+	i = 1;
+	gettimeofday(&game->start_game_time, NULL); 
+	game->philo = (t_philo *)ft_calloc(1, sizeof(t_philo));
+	aux = game->philo;
+	if (!game->philo)
+			return (free_all(game), 0);
+	game->philo->id = 1;
+	while ((long)i < game->num_philos)
+	{
+		aux = (t_philo *)ft_calloc(1, sizeof(t_philo));
+		if (!aux)
+			return (free_all(game), 0);
+		aux->id = i + 1;
+		last = give_me_last(game);
+		last->next = aux;
+		aux->prev = last;
+		i++;
+	}
+	aux->next = game->philo;
+	game->philo->prev = aux;
+	return (1);
+}
+
+void	init_game_rules(t_game *game, int argc, char **argv, int *error)
+{
+	game->num_philos = ft_atol(argv[1], error);
+	game->time_2_die = ft_atol(argv[2], error);
+	game->time_2_eat = ft_atol(argv[3], error);
+	game->time_2_sleep = ft_atol(argv[4], error);
+	if (argc == 6)
+	{
+		game->num_times_2_eat = ft_atol(argv[5], error);
+		if (game->num_times_2_eat < 0)
+		{
+			*error = 1;
+			printf("Error: Only positive arguments allowed\n");
+			return ;
+		}
+	}
+	else
+		game->num_times_2_eat = -1;
+	if (game->num_philos <= 0 || game->time_2_die < 0 || game->time_2_eat < 0 ||
+		game->time_2_sleep < 0)
+	{	
+		*error = 1;
+		printf("Error: Only positive arguments allowed and philos > 0\n");
+	}	
+	if (game->num_philos > PHILOS_MAX)
+		*error = 1;
+	game->running = 1;
+}
+
 int	main(int argc, char **argv)
 {
-	(void)argv;
+	t_game	game;
+	int		error;
+	t_philo *aux;//
+
+	error = 0;
 	if (argc < 5 || argc > 6)
 	{
 		printf("please: ./philo num_philos time_2die time_2eat time_2sleep\n");
 		printf("you can add another arg at the end of num_times_philo_eat\n");
 		return (1);
 	}
-	
-		
-
-
+	init_game_rules(&game, argc, argv, &error);
+	if (error)
+	{
+		printf("Error: num_philos > %d. Args must be LONG range", PHILOS_MAX);
+		return (1);
+	}
+	if (!init_philo(&game))
+		return (1);
+	while (game.running)
+	{
+		aux = game.philo;
+		printf("%d\n", aux->id);
+		aux = aux->prev;
+		while (aux->id != 1)
+		{
+			printf("%d\n", aux->id);
+			aux = aux->prev;
+		}
+		game.running = 0;
+	}
+	free_all(&game);
+	return (0);
 }
--- 03_Philosophers/philo/philo.h
+++ 03_Philosophers/philo/philo.h
@@ -6,21 +6,25 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 12:25:59 by jrollon-          #+#    #+#             */
-/*   Updated: 2025/04/08 21:25:23 by jrollon-         ###   ########.fr       */
+/*   Updated: 2025/04/09 17:53:39 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+# include <sys/time.h>
 # include <pthread.h>
+# include <limits.h>
 # include <stdlib.h>
 # include <stdio.h>
+
+# define PHILOS_MAX 250
 
 typedef struct		s_philo
 {
 	int				id;
 	pthread_t       thread;
-	long int		last_eat_time;
+	struct timeval	last_eat_time;
 	pthread_mutex_t	*fork;
 	struct s_philo	*next;
 	struct s_philo	*prev;
@@ -28,16 +32,19 @@ typedef struct		s_philo
 
 typedef struct		s_game
 {
-	int				num_philos;
-	long			time_to_die;
-	long			time_to_eat;
-	long			time_to_sleep;
-	int				num_times_to_eat;
+	long			num_philos;
+	long			time_2_die;
+	long			time_2_eat;
+	long			time_2_sleep;
+	long			num_times_2_eat;
+	int				running;
 	pthread_mutex_t	writing;
+	struct timeval	start_game_time;
 	t_philo			*philo;
 }					t_game;
 
-void	*ft_calloc(size_t nmemb, size_t size);
-int		ft_atoi(const char *nptr);
+void		*ft_calloc(size_t nmemb, size_t size);
+long int	ft_atol(char *nptr, int *error);
+void		free_all(t_game *game);
 
 #endif
