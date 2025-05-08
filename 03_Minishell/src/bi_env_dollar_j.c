74af22bcd7150c20d28de2857ae5972235ca4d2f
Javier Rollon <J4p032@gmail.com>
2025-05-09 00:25:58 +0200

    fix split bug echo expand ok

--- 03_Minishell/src/bi_env_dollar_j.c
+++ 03_Minishell/src/bi_env_dollar_j.c
@@ -6,7 +6,7 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 14:14:52 by jrollon-          #+#    #+#             */
-/*   Updated: 2025/05/07 18:55:21 by jrollon-         ###   ########.fr       */
+/*   Updated: 2025/05/09 00:10:01 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,45 +26,36 @@ size_t	ft_envlen(const char *str)
 size_t	ft_strlen_quoted(const char *str)
 {
 	size_t	i;
-	
+
 	i = 0;
 	while (str[i] && str[i] != ' ')
 		i++;
 	return (i);
 }
 
-
 /*if not found will return 0, if found will return the number of env variable*/
 /*can be the same up to size of env (from 0 to =), but str can be more so...*/
-/*...not valid. Ex: $USERpotato. USER is 4 but USERpotato more*/
+/*...not valid unless end $. Ex: $USERpotato. USER is 4 but USERpotato more...*/
+/*...$USER$ is valid and $USER$USER$ also*/
 int	valid_env(const char *str, t_input *in, size_t w)
 {
 	size_t	n;
 	size_t	envlen;
-	
+	int		dqu;
+
 	n = 0;
 	if (!str)
 		return (-1);
-	if (in->status[w] == DQUO_SP || in->status[w] == DQUO_NSP)
-	{
-		while (in->envp[n])
-		{
-			envlen = ft_envlen(in->envp[n]);
-			if (!ft_strncmp(in->envp[n], str, envlen))
-			{
-				if (ft_strlen_quoted(str) == envlen)
-					return (n);	
-			}
-			n++;
-		}
-	}
+	dqu = (in->status[w] == DQUO_SP || in->status[w] == DQUO_NSP);
 	while (in->envp[n])
 	{
 		envlen = ft_envlen(in->envp[n]);
-		if (ft_strnstr(in->envp[n], str, envlen))
+		if (!ft_strncmp(in->envp[n], str, envlen))
 		{
-			if (ft_strlen(str) == envlen)
-				return (n);	
+			if (dqu && (ft_strlen_quoted(str) == envlen || str[envlen] == '$'))
+				return (n);
+			else if (!dqu && (ft_strlen(str) == envlen || str[envlen] == '$'))
+				return (n);
 		}
 		n++;
 	}
