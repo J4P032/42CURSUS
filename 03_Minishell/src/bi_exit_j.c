b430d37dc3bd8cc7605f7e8d06f78a9da0a99043
Javier Rollon <J4p032@gmail.com>
2025-05-18 21:08:49 +0200

    haciendo EXIT

--- 03_Minishell/src/bi_exit_j.c
+++ 03_Minishell/src/bi_exit_j.c
@@ -6,15 +6,42 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 11:16:14 by jrollon-          #+#    #+#             */
-/*   Updated: 2025/05/12 19:23:19 by jrollon-         ###   ########.fr       */
+/*   Updated: 2025/05/18 20:33:26 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell_m.h"
 #include "../inc/minishell_j.h"
 
+/*with error 1 from too many arguments bash dont exit with 2 yes it does*/
+void	exit_miniyo(t_input *in, int num_exit)
+{
+	if (num_exit == 2)
+		ft_printf("miniyo: exit: %s: numeric argument required\n", in->args);
+	clean_all(in, num_exit);
+	if (num_exit == 0)
+		rl_clear_history();
+	exit(num_exit);	
+}
+
+
 void	ft_exit(t_input *in)
 {
-	(void)in;
-	exit(0);
+	long long	arg;
+	int			error;
+	
+	error = 0;
+	if (in->args[0])
+		arg = ft_atol(in->args, &error);
+	else
+		arg = 0;
+	if (error)
+		exit_miniyo(in, 2);
+	else
+	{
+		if (!ft_strncmp(in->args, in->parsed, ft_strlen(in->parsed)))
+			exit_miniyo(in, arg);
+		else
+			ft_printf("miniyo: exit: too many arguments\n");
+	}
 }
