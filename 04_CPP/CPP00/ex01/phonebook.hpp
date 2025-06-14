c566c78d187f3d744e487a9fe222058b1c0a5d26
Javier Rollon <J4p032@gmail.com>
2025-06-14 22:04:08 +0200

    PhoneBook EXIT Y ADD

---
+++ 04_CPP/CPP00/ex01/phonebook.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/14 21:24:12 by jrollon-          #+#    #+#             */
+/*   Updated: 2025/06/14 21:25:08 by jrollon-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include <iostream>
+
+void	enterContact(PhoneBook *phonebook, Contact *contact);
+
+#endif
