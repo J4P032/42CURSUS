5c2ff50497ece02ef80a28d921b6338c2ef082b8
Javier Rollon <J4p032@gmail.com>
2025-06-27 23:31:09 +0200

    horda negativa fix

--- 04_CPP/CPP01/ex01/main.cpp
+++ 04_CPP/CPP01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 22:57:49 by jrollon-          #+#    #+#             */
-/*   Updated: 2025/06/27 23:05:59 by jrollon-         ###   ########.fr       */
+/*   Updated: 2025/06/27 23:29:50 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int main(void)
 {
     Zombie  	*horde;
-    int     	num_zombies = 8;
+    int     	num_zombies = 10;
     std::string	zombie_name = "Paco";
 
     horde = zombieHorde(num_zombies, zombie_name);
--- 04_CPP/CPP01/ex01/zombieHorde.cpp
+++ 04_CPP/CPP01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 22:28:51 by jrollon-          #+#    #+#             */
-/*   Updated: 2025/06/27 23:08:59 by jrollon-         ###   ########.fr       */
+/*   Updated: 2025/06/27 23:28:45 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ Zombie *zombieHorde(int N, std::string name)
 {
     Zombie *horde;
     
+    if (N <= 0)
+        return (NULL);
     horde = new Zombie[N];
     for (int i = 0; i < N; i++){
         horde[i].setName(name);
