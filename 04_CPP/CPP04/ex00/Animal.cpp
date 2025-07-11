8157a71f19cfa26454d0800c307703bddc010ef4
Javier Rollon <J4p032@gmail.com>
2025-07-11 20:55:26 +0200

    Ex00 de CPP04

---
+++ 04_CPP/CPP04/ex00/Animal.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/11 16:37:35 by jrollon-          #+#    #+#             */
+/*   Updated: 2025/07/11 20:47:58 by jrollon-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+
+Animal::Animal(void) : _type("any"){
+	std::cout << "🛠️	" << GREEN"Animal Default Constructor Called" RESET << std::endl;
+}
+
+Animal::Animal(const Animal &other) : _type(other._type){
+	std::cout << "🐑	" << GREEN"Animal Copy Constructor Called" RESET << std::endl;
+}
+
+Animal::~Animal(void){
+	std::cout << "💣	" << RED"Animal Destructor Called" RESET << std::endl;
+}
+
+Animal	&Animal::operator=(const Animal &other){
+	_type = other._type;
+	return (*this);
+}
+
+std::string	Animal::getType(void) const{
+	return (_type);
+}
+
+void	Animal::makeSound(void) const{
+	std::cout << "🔇	" << YELLOW "I mute my sound because I am not any type of Animal" RESET << std::endl;
+}
