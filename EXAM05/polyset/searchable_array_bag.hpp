6d6af7938e35822edc6f7a771fd6dc4a35b1504f
JRM32 <Javier.rollon@gmail.com>
2026-03-02 12:07:47 +0100

    empezando polyset

---
+++ EXAM05/polyset/searchable_array_bag.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   searchable_array_bag.hpp                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/02 12:02:02 by jrollon-          #+#    #+#             */
+/*   Updated: 2026/03/02 12:07:24 by jrollon-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SEARCHABLE_ARRAY_BAG_HPP
+# define SEARCHABLE_ARRAY_BAG_HPP
+
+# include "array_bag.hpp"
+
+class searchable_tree_bag : public array_bag{
+	
+};
+
+
+#endif
---
+++ EXAM05/polyset/searchable_tree_bag.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   searchable_tree_bag.hpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/02 12:03:17 by jrollon-          #+#    #+#             */
+/*   Updated: 2026/03/02 12:06:37 by jrollon-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SEARCHABLE_TREE_BAG_HPP
+# define SEARCHABLE_TREE_BAG_HPP
+
+# include "tree_bag.hpp"
+
+class searchable_tree_bag : public tree_bag{
+	
+};
+
+
+#endif
