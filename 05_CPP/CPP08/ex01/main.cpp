554bcd2b07499ff9d7654d832657c2354661e7ee
J4P032 <J4p032@gmail.com>
2026-03-12 15:41:57 +0100

    add test of subject

--- 05_CPP/CPP08/ex01/main.cpp
+++ 05_CPP/CPP08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 10:40:33 by jrollon-          #+#    #+#             */
-/*   Updated: 2026/03/12 15:37:27 by jrollon-         ###   ########.fr       */
+/*   Updated: 2026/03/12 15:41:04 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,17 +30,29 @@ void	fill_vector(std::vector<int>& v, size_t n){
 
 int	main(void){
 	try{
+		//test de subject
+		std::cout <<  "----- TEST of subject  ----" << std::endl;
+		Span sp = Span(5);
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+
 		
 		//prueba de extremos
-		std::cout << "----- TEST of borders a(5)  ----" << std::endl;
+		std::cout << std::endl <<  "----- TEST of borders a(5)  ----" << std::endl;
 		Span a(5);
 		a.addNumber(2147483647);
 		a.addNumber(0);
 		a.addNumber(-2147483648);
-		
 		std::cout << a << std::endl;
 		std::cout << "Longest Span of a(5): " << a.longestSpan() << std::endl;
 		std::cout << "Shortest Span of a(5): " << a.shortestSpan() << std::endl;
+	
+		
 		
 		//prueba con 10 numeros para ver si lo hace bien. Los imprimo para comprobarlo
 		std::cout << std::endl << "----- TEST WITH 10 ints b(10) ----" << std::endl;
@@ -53,6 +65,9 @@ int	main(void){
 		std::cout << b << std::endl;
 		std::cout << "Longest Span of b(10): " << b.longestSpan() << std::endl;
 		std::cout << "Shortest Span of b(10): " << b.shortestSpan() << std::endl;
+	
+	
+	
 	
 		//prueba de union de a y b
 		std::cout << std::endl << "----- TEST WITH 10 ints but with join with a(5) ----" << std::endl;
@@ -73,7 +88,9 @@ int	main(void){
 		std::cout << "Longest Span of d(10): " << d.longestSpan() << std::endl;
 		std::cout << "Shortest Span of d(10): " << d.shortestSpan() << std::endl; 
 	
-		//PRUEBA CON 10000
+	
+	
+		//PRUEBA CON 25000
 		std::cout << std::endl << "----- TEST WITH 25000 e(25000) ----" << std::endl;
 		Span e(25000);
 		aux.clear();
@@ -81,6 +98,8 @@ int	main(void){
 		e.addNumber(aux.begin(), aux.end());
 		std::cout << "Longest Span of e(25000): " << e.longestSpan() << std::endl;
 		std::cout << "Shortest Span of e(25000): " << e.shortestSpan() << std::endl;
+	
+	
 	
 		//fallo de suma con reserva mayor a la permitida
 		std::cout << std::endl << "----- TEST exceed of operator+ ----" << std::endl;
