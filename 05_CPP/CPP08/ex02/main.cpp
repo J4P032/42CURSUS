9102ba623c35905e8f7fbc990e7caec1aeab9de5
J4P032 <J4p032@gmail.com>
2026-03-13 12:56:58 +0100

    mutantstack completo

--- 05_CPP/CPP08/ex02/main.cpp
+++ 05_CPP/CPP08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/13 09:38:28 by jrollon-          #+#    #+#             */
-/*   Updated: 2026/03/13 12:11:27 by jrollon-         ###   ########.fr       */
+/*   Updated: 2026/03/13 12:56:26 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,19 @@
 #include <vector>
 #include <ctime>
 #include <cstdlib>
+#include <list>
 
+/*Un STACK como digo en el hpp es una capa que hace comportarse al contenedor por
+debajo diferente a como debería. En este ejercicio cuando se hace una introducción
+de 5 y luego 17, en el STACK queda como (17,5) PEEEEROOO internamente el DEQUE, VECTOR
+o LIST queda como (5,17). Entonces cuando se pide un .top() este método interno lo que
+devuelve es el contenedor.back() y así PARECE que el contenedor los tiene almacenados
+en inverso.. PERO NO!!
+POR ESOOO cuando se aplica el iterador a MutantStack pasa que sí que se accede al 
+contenedor por debajo (DEQUE, LIST o VECTOR) y por lo tanto lo muestra en el orden suyo
+que parece que es invertido PERO NO. asi saldrá en la solución a este primer test main_subject
+como: 5,3,5,737,0 aunque haya sido el 0 el último número en meterse. Si lo hubieramos sacado
+con .top() si que lo mostraría. se lo añado al final a las pruebas de subject para demostrarlo*/
 int main_subject(){
 	MutantStack<int> mstack;
 	mstack.push(5);
@@ -37,6 +49,33 @@ int main_subject(){
 	++it;
 	}
 	std::stack<int> s(mstack);
+	std::cout << "con pop saldrá el cero: " << mstack.top() << std::endl;
+	return 0;
+}
+
+int main_list(){
+	std::list<int> mstack;
+	mstack.push_back(5); //push() = push_back()
+	mstack.push_back(17); 
+	std::cout << mstack.back() << std::endl; //top() = back()
+	mstack.pop_back();
+	std::cout << mstack.size() << std::endl;
+	mstack.push_back(3);
+	mstack.push_back(5);
+	mstack.push_back(737);
+	//[...]
+	mstack.push_back(0);
+	std::list<int>::iterator it = mstack.begin();
+	std::list<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+	std::cout << *it << std::endl;
+	++it;
+	}
+	std::list<int> s(mstack);
+	std::cout << "con pop saldrá el cero: " << s.back() << std::endl;
 	return 0;
 }
 
@@ -69,7 +108,7 @@ int	main_mytests(){
 	srand(time(NULL));
 	
 	//PRUEBA CON VECTOR de INTs
-	std::cout << "VECTOR<int> TEST" << std::endl;
+	std::cout << "\n\nVECTOR<int> TEST" << std::endl;
 	std::vector<int> aux;
 	fill_container(aux, 10);
 	std::cout << "vector<int> formado:" << std::endl;
@@ -103,24 +142,42 @@ int	main_mytests(){
 
 	//LIST of abcd
 	std::cout << "\nMutantStack<char> in a LIST" << std::endl;
-	
-	
+	std::list<char> aux2;
+	for (char i = 'a'; i != '{'; i++){
+		aux2.push_back(i);
+	}
+	std::cout << "list container:" << std::endl;
+	print_container(aux2);
+	std::cout << "MutantStack<char> composed but reverse order:" << std::endl;
+	MutantStack<char> d(aux2.rbegin(), aux2.rend());
+	std::cout << d << std::endl;
 	
 	return 0;
 }
 
 
-int main(int c, char** v){
+int main(){
 	int r;
+	int ret;
 	
-	if (c != 2){
-		std::cout << "usage: mutantStack <1 or 2> 1=main of subject, 2=my tests" << std::endl;
-		return 1;
+	std::cout << "TESTS -----" << std::endl;
+	std::cout << "1. Subject test\n2. Subject test with std::list\n3. My own tests" << std::endl;
+	std::cout << "choose an option: ";
+	std::cin >> r;
+ 
+	switch (r){
+		case 1:
+			ret = main_subject();
+			break;
+		case 2:
+			ret = main_list();
+			break;
+		case 3:
+			ret = main_mytests();
+			break;
+		default:
+			break;	
 	}
-	if (std::string(v[1]) == "1")
-		r = main_subject();
-	else
-		r = main_mytests();
 	
-	return r;
+	return ret;
 }
