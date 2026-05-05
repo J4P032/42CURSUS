5537a2901ce4543c0f4febe97f6a8351f7544ec5
Javier Rollon <J4p032@gmail.com>
2026-05-05 16:16:28 +0200

    Final 100%

--- 05_Inception/srcs/requirements/mariadb/tools/setup.sh
+++ 05_Inception/srcs/requirements/mariadb/tools/setup.sh
@@ -3,7 +3,7 @@
 service mariadb start
 sleep 2
 
-# Usamos las variables SQL_DATABASE, SQL_USER, SQL_PASSWORD y SQL_ROOT_PASSWORD del .env
+# Usamos las variables del .env
 # mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 # mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 # mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
@@ -11,22 +11,26 @@ sleep 2
 # mariadb -e "FLUSH PRIVILEGES;"
 
 # 1. Creamos la base de datos
+# mariadb es un comando que sustituye al antiguo mysql
+# -e es para que ejecute la sentencia SQL que está entre comillas sin entrar en modo interactivo.
+# SQL va como: ACCION - OBJETO - CONDICION - con final en ';'. las \`(`), garantizan preservar los caractereres especiales o espacios.
 mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
-# 2. Creamos el usuario para CUALQUIER conexión ('%') y le damos permisos
-# Esto es lo que usará WordPress para conectar desde su contenedor
+# 2. Creamos el usuario para CUALQUIER conexión ('%') y le damos permisos SOLO a la base de datos $SQL_DATABASE. .* son todas las tablas dentro
+# Esto (%) es por que el usuario de wordpress se conecta desde FUERA (desde el contenedor de wordpress)
 mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';"
 
-# 3. Cambiamos la contraseña de root (usamos 'echo' para evitar errores si ya está cambiada)
+# 3. Cambiamos la contraseña de root. En realidad le damos una, por que mariadb crea el root SIN contraseña. Asi más seguridad.
+# aquí es localhost, por que el root SIEMPRE es local host. No sería % (desde fuera)
 mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
 # 4. Refrescamos y apagamos para reiniciar con 'exec'
 mariadb -p${SQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
 
-# exec consigue que herede el PID1 y mate el shell secundario haciendolo PID1 tambien
+# exec consigue que herede el PID1 y mate el shell secundario haciendolo PID1 también
 # docker solo escucha señales en PID1
-# mysqld_safe es el proceso mara para lanzar mariadb. Safe...
+# mysqld_safe es el proceso para para lanzar mariadb. Safe...
 # ...para relanzarlo en caso de problema. Ya está definido para que no sea un daemon por defecto.
 exec mysqld_safe
--- 05_Inception/srcs/requirements/wordpress/tools/setup.sh
+++ 05_Inception/srcs/requirements/wordpress/tools/setup.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
-# Esperamos a que MariaDB este lista
+# Esperamos a que MariaDB este lista. 'wp' es el CLI wp-cli.phar que hemos acortado con el comando move en el dockerfile
 sleep 10
 
 if [ ! -f /var/www/wordpress/wp-config.php ]; then
+    # el que no exista el wp-config.php es que no se ha instalado. El core Download es como bajar el .zip de worpress
+    # el --allow-root es necesario en todos por que por defecto al root de linux no se le permite ejecutar 'wp' en wordpress
     wp core download --allow-root
 
+    # Generamos el archivo de configuración wp-config.php que conecta wordpress con MariaDB.
+    # --dbhost=mariadb:3306 dice que busque en el contenedor que en el docker-compose se llama mariadb. 
     wp config create \
         --dbname=$SQL_DATABASE \
         --dbuser=$SQL_USER \
         --dbpass=$SQL_PASSWORD \
         --dbhost=mariadb:3306 --allow-root
 
+    # rellena las tablas de la base de datos de MariaDB. Aquí se crea el administrador y abajo el usuario.
     wp core install \
         --url=$DOMAIN_NAME \
         --title=$SITE_TITLE \
@@ -20,6 +25,7 @@ if [ ! -f /var/www/wordpress/wp-config.php ]; then
         --admin_email=$WP_ADMIN_EMAIL \
         --allow-root
 
+    # role=author hace que no sea administrador.
     wp user create \
         $USER_LOGIN $USER_EMAIL \
         --role=author \
@@ -28,6 +34,7 @@ if [ ! -f /var/www/wordpress/wp-config.php ]; then
 fi
 
 # Cambiamos el dueño a www-data de forma recursiva
+# www-data:www-data (usuario:grupo) definidos en el www.conf
 chown -R www-data:www-data /var/www/wordpress
 # Aseguramos permisos de escritura para el grupo
 chmod -R 775 /var/www/wordpress/wp-content
